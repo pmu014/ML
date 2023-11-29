b963d12ccf65557813212797ca20a70a2ff8827c
Minuk <ansems@naver.com>
2023-11-29 13:51:22 +0900

    fix: mainPage title

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
 import './App.css';
 import logo from './dalpenguin.png';
 
+function Tab({ title }) {
+  return (
+    <div className="tabBoxStyle">
+      {title}
+    </div>
+  );
+}
+
 function App() {
-  const style = {
-    height: "120px",
-    fontSize: "50px",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "25px",
-  };
-  
-  const logoStyle = {
-    height: "90px",
-    padding: "20px",
-  }
-
-  const tabStyle = {
-    height: "80px",
-    fontSize: "30px",
-    display: "flex",
-    justifyContent: "center",
-  };
-
-  const tabBoxStyle = {
-    width: "250px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: "25px",
-    border: "1px solid black",
-  };
-
   return (
     <div>
-      <main style={style}>
-        <img src={logo} style={logoStyle}/>
+      <main className='mainStyle'>
+        <img src={logo} className='logoStyle'/>
         Minuk's Life
       </main>
-      <section style={tabStyle}>
-        <div style={tabBoxStyle}>
-          Content
-        </div>
-        <div style={tabBoxStyle}>
-          Sports
-        </div>
-        <div style={tabBoxStyle}>
-          Health
-        </div>
-        <div style={tabBoxStyle}>
-          Diary
-        </div>
+      <section className='tabStyle'>
+        <Tab title="Content" />
+        <Tab title="Sports" />
+        <Tab title="Health" />
+        <Tab title="Diary" />
       </section>
     </div>
   );
